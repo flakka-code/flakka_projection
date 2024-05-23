fda25a18e4181382d810d60332a8c9877e4c28f6
ryanhanks-bestow <ryan.hanks@bestow.world>
2024-05-23 18:53:42 -0500

    initial commit

---
+++ flakka_projection/lib/flakka_projection.dart
@@ -0,0 +1,4 @@
+/// Behavior to project effect-value snapshot from a journal to a store
+library;
+
+export 'src/flakka_projection.dart';
---
+++ flakka_projection/lib/src/flakka_projection.dart
@@ -0,0 +1,7 @@
+/// {@template flakka_projection}
+/// Behavior to project effect-value snapshot from a journal to a store
+/// {@endtemplate}
+class FlakkaProjection {
+  /// {@macro flakka_projection}
+  const FlakkaProjection();
+}
